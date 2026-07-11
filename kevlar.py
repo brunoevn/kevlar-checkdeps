8482b9d2cc5bdcdd94b6b97f51305cf6508d4d70
Bruno Nielsen <brunovn@gmail.com>
2026-07-11 19:09:44 -0300

    chore: bump version to 1.8.3

--- kevlar.py
+++ kevlar.py
@@ -44,7 +44,7 @@ class SafeWriter:
 sys.stdout = SafeWriter(sys.stdout)
 sys.stderr = SafeWriter(sys.stderr)
 
-VERSION = "1.8.2"
+VERSION = "1.8.3"
 
 # External APIs Configuration
 URL_NPM_REGISTRY = "https://registry.npmjs.org/"
